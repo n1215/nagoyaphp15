7635145bf1b0f8a5785b69e72c6dc9a339cdd539
Takashi Kanemoto <ttskch@gmail.com>
2019-04-10 05:08:20 +0900

    impl smartly

--- src/Dokaku15.php
+++ src/Dokaku15.php
@@ -8,7 +8,17 @@ class Dokaku15
 {
     public function run(string $input): string
     {
-        // implement me
-        return '3445';
+        $lines = array_values(array_filter(range(0, 7), function (int $i) use ($input) {
+            return str_split(sprintf('%b', $input))[$i] ?? false;
+        }));
+
+        $vertices = [];
+        for ($i = 0; $i < count($lines); $i++) {
+            $distance = ($lines[($i + 1) % count($lines)] - $lines[$i] + 8) % 8;
+            $vertices[] = ($distance === 4 ? 2 : 3) + $distance - 1;
+        }
+        sort($vertices);
+
+        return implode('', $vertices);
     }
 }
